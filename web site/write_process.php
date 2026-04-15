a1856b1a0af272c899a784541f76d66d3c00bf59
Hong-ki-su <gisuhong258@gmail.com>
2026-04-15 17:11:41 +0900

    수정

--- web site/write_process.php
+++ web site/write_process.php
@@ -4,22 +4,27 @@ session_start();
 include 'db.php'; 
 
 if (!isset($_SESSION['userid'])) {
-    exit('접근 권한이 없습니다.');
+    echo "<script>alert('접근 권한이 없습니다.'); location.href='login.php';</script>";
+    exit;
 }
 
+if (isset($_POST['post_content']) && isset($_POST['author'])) {
+    
+    $post_content = mysqli_real_escape_string($conn, $_POST['post_content']);
+    $author = mysqli_real_escape_string($conn, $_POST['author']);
 
-$post_content = mysqli_real_escape_string($conn, $_POST['post_content']);
-$author = mysqli_real_escape_string($conn, $_POST['author']);
+    $sql = "INSERT INTO team_board (post, author, reg_date) VALUES ('$post_content', '$author', NOW())";
 
-$sql = "INSERT INTO team_board (post, author) VALUES ('$post_content', '$author')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "<script>
-        alert('글이 성공적으로 등록되었습니다.');
-        location.href = 'board.php';
-    </script>";
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>
+            alert('글이 성공적으로 등록되었습니다.');
+            location.href = 'board.php';
+        </script>";
+    } else {
+        echo "DB 에러: " . mysqli_error($conn);
+    }
 } else {
-    echo "에러: " . mysqli_error($conn);
+    echo "<script>alert('잘못된 접근입니다.'); location.href='board.php';</script>";
 }
 
 mysqli_close($conn);
